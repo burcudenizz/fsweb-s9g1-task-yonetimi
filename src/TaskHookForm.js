a880d9b14fe99afc99db10b552f29c7690510ff4
Burcu Deniz <122237317+burcudenizz@users.noreply.github.com>
2023-04-17 16:01:57 +0300

    checkbox kısmı kaldı.

--- src/TaskHookForm.js
+++ src/TaskHookForm.js
@@ -10,11 +10,77 @@ export default function TaskHookForm() {
     mode: "onChange",
   });
 
+  function gonder(data) {
+    console.log("servera gönder: ", data);
+  }
+
+  console.log("hatalar", errors);
+
   return (
-    <div>
-      Formunuzu react-hook-form kullanarak burada oluşturun. TaskForm
-      dosyasındaki HTML yapısını vs app.css içerisindeki classları
-      kullanabilirsiniz.
-    </div>
+    <form className="taskForm" onSubmit={handleSubmit(gonder)}>
+      <div className="form-line">
+        <label className="input-label" htmlFor="title">
+          Başlık
+        </label>
+        <input
+          className="input-text"
+          id="title"
+          name="title"
+          type="text"
+          {...register("title", {
+            required: "Task başlığı yazmalısınız",
+            min: {
+              value: 3,
+              message: "Task başlığı en az 3 karakter olmalı",
+            },
+          })}
+        />
+        <p className="input-error">{errors.title.message}</p>
+      </div>
+
+      <div className="form-line">
+        <label className="input-label" htmlFor="description">
+          Açıklama
+        </label>
+        <textarea
+          className="input-textarea"
+          rows="3"
+          id="description"
+          name="description"
+          {...register("description", {
+            required: "Task açıklaması yazmalısınız",
+            min: {
+              value: 10,
+              message: "Task açıklaması en az 10 karakter olmalı",
+            },
+          })}
+        ></textarea>
+        <p className="input-error">{errors.description.message}</p>
+      </div>
+
+      <div className="form-line">
+        <label className="input-label">İnsanlar</label>
+        <div>
+          {kisiler.map((p) => (
+            <label className="input-checkbox" key={p}>
+              <input
+                type="checkbox"
+                name="people"
+                value={p}
+                onChange={handleCheckboxChange}
+                checked={formData.people.includes(p)}
+              />
+              {p}
+            </label>
+          ))}
+        </div>
+        <p className="input-error">{errors.people.message}</p>
+      </div>
+      <div className="form-line">
+        <button className="submit-button" type="submit" disabled={!isValid}>
+          Kaydet
+        </button>
+      </div>
+    </form>
   );
 }
